0edc2a09ce66021643cac90341a474e599f701e4
thibaud <thibaud>
2024-09-18 10:30:39 +0200

    added CPP07/ex00

---
+++ CPP07/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thibaud <thibaud@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 10:25:24 by thibaud           #+#    #+#             */
+/*   Updated: 2024/09/18 10:27:01 by thibaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.template.hpp"
+#include <iostream>
+
+int	main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
---
+++ CPP07/ex00/whatever.template.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.template.hpp                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thibaud <thibaud@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 10:18:55 by thibaud           #+#    #+#             */
+/*   Updated: 2024/09/18 10:24:51 by thibaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_TEMPLATE_HPP
+# define WHATEVER_TEMPLATE_HPP
+
+template< typename Type >
+void	swap(Type x, Type y) {
+	Type	temp;
+	temp = x;
+	x = y;
+	y = temp;
+	return ;
+}
+
+template< typename Type >
+Type	min(Type x, Type y) {
+	return ((y<=x) ? (y) : (x));
+}
+
+template< typename Type >
+Type	max(Type x, Type y) {
+	return ((y>=x) ? (y) : (x));
+}
+
+#endif
