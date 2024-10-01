db2c67841467abce6af8310255d5e67bbcca5917
Thibaud Mouche <tmouche@z4r4p3.42lyon.fr>
2024-10-01 12:04:34 +0200

    started iterator

--- CPP08/ex02/MutantStack.template.hpp
+++ CPP08/ex02/MutantStack.template.hpp
@@ -6,12 +6,13 @@
 /*   By: tmouche <tmouche@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 18:20:04 by tmouche           #+#    #+#             */
-/*   Updated: 2024/09/29 18:54:54 by tmouche          ###   ########.fr       */
+/*   Updated: 2024/10/01 11:55:35 by tmouche          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_TEMPLATE_HPP
 # define MUTANTSTACK_TEMPLATE_HPP
+# include <stack>
 
 template <typename T>
 class MutantStack {
@@ -29,6 +30,42 @@ public :
 		}
 		return *this;
 	}
+
+	bool	empty( void ) const {
+		return this->myStack.empty();
+	}
+
+	sizet	size( void ) const {
+		return this->myStack.size();
+	}
+
+	T&	top( void ) const {
+		return this->myStack.top();
+	}
+
+	void	push(T const & value) {
+		this->myStack.push(value);
+		return ;
+	}
+
+	void	emplace(T&& value) {
+		return this->myStack.emplace(value);
+	}
+
+	void	pop( void ) {
+		this->myStack->pop();
+		return ;
+	}
+
+	void	swap(std::stack<T> const & value) {
+		this->myStack.swap(value);
+		return ;
+	}
+
+	
+	
+private :
+	std::stack<T>	myStack;
 };
 
 #endif
