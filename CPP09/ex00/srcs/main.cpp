41b01ceca96d5dc2e72b28269baf542013ad4ca8
thibaud <thibaud@inception.fr>
2025-03-23 12:00:19 -0400

    Makefile fixed CPP09

--- CPP09/ex00/srcs/main.cpp
+++ CPP09/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thibaud <thibaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 16:31:10 by thibaud           #+#    #+#             */
-/*   Updated: 2025/02/06 16:47:22 by thibaud          ###   ########.fr       */
+/*   Updated: 2025/03/23 11:54:15 by thibaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 #include "Exception.class.hpp"
 
 int main(int ac, char** av) {
-	
-	if (ac != 2)
+	if (ac != 2) {
+		std::cout << "Need a wallet has input.." << std::endl;
 		return 1;
+	}
 	try {
 		BitcoinExchange	test("data.csv");
 		test.valorise(av[1]);
